e8f9c69d933256286bbe5f20a4fd5ce67132c0ab
xysj1989 <xysj1989@126.com>
2021-02-05 14:12:04 -0500

    Create z-0-Types.sh

---
+++ z-0-Types.sh
@@ -0,0 +1,22 @@
+####### 001 miRNA
+
+1 Selection of reference genes for miRNA qRT-PCR under abiotic stress in grapevine
+2 Selection of reference genes is critical for miRNA expression analysis in human cardiac tissue. A focus on atrial fibrillation
+3 A strategy to find suitable reference genes for miRNA quantitative PCR analysis and its application to cervical specimens
+
+
+
+####### 002 circular RNA
+
+
+####### 003 Circulating miRNA
+
+
+
+
+
+
+
+
+
+
