88320a9c1a02f957a329c629b3b6e3fe1df7d6b2
xysj1989 <xysj1989@126.com>
2021-02-05 21:47:25 -0500

    Create z-1-Script.py

---
+++ z-1-Script.py
@@ -0,0 +1,9 @@
+#1 安装BeautifulSoup                       pip3 install beautifulsoup4
+#2 安装第三方html解析器lxml                  pip3 install lxml
+#3 安装纯Python实现的html5lib解析器          pip3 install html5lib
+
+
+
+
+
+
